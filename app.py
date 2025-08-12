04674a2274a006c4febd1f66780ff83b27b57069
Dumindu Jayasekara <jayedumindu@gmail.com>
2025-08-12 16:53:44 +0530

    PORT

--- app.py
+++ app.py
@@ -538,9 +538,11 @@ async def global_exception_handler(request: Request, exc: Exception):
     )
 
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
-        "main:app",  # Replace "main" with your filename
+        "main:app",  # Replace "main" with your filename (without .py)
         host="0.0.0.0",
-        port=8000,
+        port=port,
         reload=True
     )
