3445d16e32277bbfa4a6b4f0528eda77165829e1
Dumindu Jayasekara <jayedumindu@gmail.com>
2025-08-14 21:04:44 +0530

    noice: kelauna

--- app.py
+++ app.py
@@ -557,7 +557,7 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
-        "main:app",  # Replace "main" with your filename (without .py)
+        "app:app",  # Replace "main" with your filename (without .py)
         host="0.0.0.0",
         port=port,
         reload=True
--- updated_backend.py
+++ updated_backend.py
@@ -939,7 +939,7 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
-        "main:app",
+        "updated_backend:app",
         host="0.0.0.0",
         port=port,
         reload=False
